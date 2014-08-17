1504a7040a0f106d15ab5f7675ae83b3c26d76c1
Penny Chase <mpchase@mac.com>
2014-08-17 11:23:10 -0500

    Initial commit

---
+++ Plot1.R
@@ -0,0 +1,15 @@
+# Set working diectory
+setwd("~/Documents/MOOCs/Data Science Specialization/Course4_Exploratory-Data-Analysis/Projects/ExData_Plotting2")
+
+# Assume the data has been downloaded to exdata-data-NEI_data
+# Read in the data and the classification codes from R object files
+NEI <- readRDS("./exdata-data-NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("./exdata-data-NEI_data/Source_Classification_Code.rds")
+
+# Compute the total emissions by year
+te <- tapply(NEI$Emissions, NEI$year, sum)
+# Convert result to a data frame
+totalEmissions <- data.frame(Year=as.Date(names(te), "%Y"), Emissions=as.vector(te))
+
+# Plot as a time series: type="o" draws lines and overplotted points
+with(totalEmissions, plot(Year, Emissions, type="o", pch=20))
