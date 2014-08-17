439e72db1444c57271a4e0d5c512e6e04735108e
Penny Chase <mpchase@mac.com>
2014-08-17 18:49:33 -0500

    Added ggplot2 as alternative

--- Plot1.R
+++ Plot1.R
@@ -14,3 +14,14 @@ totalEmissions <- data.frame(Year=as.Date(names(te), "%Y"), Emissions=as.vector(
 # Plot as a time series: type="o" draws lines and overplotted points
 # Scale Emissions to thousands of tons by dividing by 1000 to make the y-axis values easier to read
 with(totalEmissions, plot(Year, Emissions/1000, type="o", pch=20, ylab="Emissions (1000s of tons)"))
+
+###
+### Using ggplot 
+###
+library(ggplot2)
+
+# Create the basic plot of year and Emissions
+n <- ggplot(NEI, aes(year, Emissions))
+# Use stat_summary() to plot the summary of the y values (i.e., the Emissions)
+# Plot the points as well as the lines to make clear which years we have data
+n + stat_summary(fun.y = "sum", geom = "line") + stat_summary(fun.y = "sum", geom = "point")
