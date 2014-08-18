3cf6787ca0efc0076c2b7aae7eef138d464d6a67
Penny Chase <mpchase@mac.com>
2014-08-18 09:24:25 -0500

    Added horiztonal line for the 1999 emissions to make the difference clearer

--- Plot3.R
+++ Plot3.R
@@ -13,10 +13,15 @@ SCC <- readRDS("./exdata-data-NEI_data/Source_Classification_Code.rds")
 baltimore <- NEI[NEI$fips == "24510",]
 
 
+# Create a data frame to add a horizontal line to each facet (showing total emissions by type in 1999)
+hline.data <- ddply(baltimore[baltimore$year==1999,], .(type), summarize, sum=sum(Emissions))
+
 # Create the ggplot plot -- tell ggplot about the data and the aesthetics mapping
 b <- ggplot(baltimore, aes(year, Emissions))
 # Summarize the emissions data and separate by type
-b + stat_summary(fun.y = "sum", geom = "line") + facet_grid(.~type)
+b <- b + stat_summary(fun.y = "sum", geom = "line") + facet_grid(.~type)
+# Add a horizontal line to show the emissions in 1999 so we can see how emissions changed between 1999-2008
+b + geom_hline(aes(yintercept=sum), colour="#9999CC", hline.data)     
 
 
 
